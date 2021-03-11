3248bd68cc2d9ed068e61718b356c4b15c971493
AngelicaFlores <floresangelicales@outlook.es>
2021-03-11 13:22:29 -0600

    11/03

---
+++ app/src/main/java/mx/ipn/cic/geo/calculadora/MainActivity.java
@@ -0,0 +1,17 @@
+package mx.ipn.cic.geo.calculadora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Objects.requireNonNull(this.getSupportActionBar()).hide();
+    }
+}
